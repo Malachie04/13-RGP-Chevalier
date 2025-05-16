2eb2162badea11584cb126a230b761f1c1addf3c
Malachie04 <malakimalachie@gmail.com>
2025-05-16 15:47:34 +0200

    SecondPush

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,74 @@
-//Classes
-class Chevalier{
-    constructor(name,strenght,magic,life,mana,position){
+
+
+//La classe Chevalier
+class Chevalier {
+    constructor(name, strength, magic) {
         this.name = name;
-        this.strenght = strenght;
+        this.strength = strength;
         this.magic = magic;
-        this.life = life;
-        this.mana = mana;
-        this.position = position;
+        this.life = 100; // Points de vie initiaux
+        this.mana = 50;  // Points de mana initiaux
+        this.potions = 2; // Nombre de potions initial
     }
-    shout(){
-        return this.name;
+
+    shout() {
+        return `Je suis ${this.name} !`;
     }
-    classicAttack(){
-        return this.strenght;
+
+    attack(target) {
+        if (this.isDead()) {
+            return `${this.name} est mort et ne peut pas attaquer !`;
+        }
+        if (target.isDead()) {
+            return `${target.name} est déjà mort !`;
+        }
+        target.getDamages(this.strength);
+        return `${this.name} attaque ${target.name} et inflige ${this.strength} dégâts !`;
     }
-    magicAttack(){
-        return 20;
+
+    magicAttack(target) {
+        if (this.isDead()) {
+            return `${this.name} est mort et ne peut pas lancer de sort !`;
+        }
+        if (this.mana < 20) {
+            return `${this.name} n'a pas assez de mana pour lancer un sort !`;
+        }
+        if (target.isDead()) {
+            return `${target.name} est déjà mort !`;
+        }
+        this.mana -= 20;
+        target.getDamages(this.magic);
+        return `${this.name} lance un sort sur ${target.name} et inflige ${this.magic} dégâts ! Mana restant : ${this.mana}`;
     }
-    getDamage(damage){
+
+    getDamages(damage) {
         this.life -= damage;
-        return this.name + " a pris " + damage + " et son état de santé est !"+ this.life;
-    }
-    userPosition(life){
-
-        if(this.position <=100){
-         this.life+=30;
+        if (this.life < 0) {
+            this.life = 0;
         }
-
+        return `${this.name} a pris ${damage} dégâts. Points de vie restants : ${this.life}`;
     }
 
-    heal(){
-        return this.name + " se soigne de " + this.life + " !";
-    }
-    manaAttack(){
-        return this.name + " attaque avec une mana de " + this.mana + " !";
-    }
-    isDead(){   
-        if(this.life <= 0){
-            return true;
-        }else{
-            return false;
+    usePotion() {
+        if (this.isDead()) {
+            return `${this.name} est mort et ne peut pas utiliser de potion !`;
         }
+        if (this.potions <= 0) {
+            return `${this.name} n'a plus de potions !`;
+        }
+        this.potions -= 1;
+        this.life += 30;
+        if (this.life > 100) {
+            this.life = 100;
+        }
+        return `${this.name} utilise une potion et restaure 30 PV. Points de vie : ${this.life}, Potions restantes : ${this.potions}`;
     }
-      
+
+    isDead() {
+        return this.life <= 0;
+    }
 }
+
+
+//Selection des composants
+
